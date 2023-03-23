3501a69a62a8c34a48069c94922dea37fbcd7e8c
spxedu <62041731+spxedu@users.noreply.github.com>
2023-03-23 17:14:38 +0700

    Add files via upload

---
+++ thuc-hanh-layout-upload/routes/index.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+var homeCtrl = require('../controllers/home.controller')
+
+router.get('/', homeCtrl.index);
+
+
+module.exports = router;
